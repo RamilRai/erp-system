9bc102b94006f1126615cf804e3d8c4e51bad946
cuman <cuman10071@gmail.com>
2023-03-17 09:59:18 +0545

    changes in footer done

--- resources/views/layouts/adminpanel/footer.blade.php
+++ resources/views/layouts/adminpanel/footer.blade.php
@@ -2,7 +2,7 @@
 <div class="nk-footer">
     <div class="container-fluid">
         <div class="nk-footer-wrap">
-            <div class="nk-footer-copyright"> &copy; 2022 DashLite. Template by <a href="https://softnio.com" target="_blank">Softnio</a>
+            <div class="nk-footer-copyright"> &copy; {{date('Y')}} DashLite. Template by <a href="https://softnio.com" target="_blank">Softnio</a>. Developed by <a href="https://cltech.com.np/" target="_blank">Code Logic Technologies Pvt. Ltd.</a>
             </div>
         </div>
     </div>
